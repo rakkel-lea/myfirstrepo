bae107e05a82a72ff10dd8e8987a53df4b39adfe
rakkel-lea <162523429+rakkel-lea@users.noreply.github.com>
2024-03-07 09:55:10 +0800

    Add files via upload

---
+++ ACTIVITY1.py
@@ -0,0 +1,27 @@
+num = int(input("Enter Grade: "))
+
+if(num > 100):
+    print("Please Input Grade 0 - 100")
+elif(num >= 90):
+    print("A+")
+elif(num >= 80):
+    print("A")
+elif(num >= 75):
+    print("B+")
+elif(num >= 70):
+    print("B")
+elif(num >= 65):
+    print("C+")
+elif(num >= 60):
+    print("C")
+elif(num >= 55):
+    print("D+")
+elif(num >= 50):
+    print("D")
+elif(num >= 0):
+    print("F")
+else:
+    print("ERROR")
+
+    
+    
